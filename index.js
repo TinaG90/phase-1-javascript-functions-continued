7920a52636ac084a12305559b81e54cae05345b2
Altenia Gaines <gaines.tina90@gmail.com>
2023-03-23 20:28:42 -0700

    Functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun()
+
+function mondayWork(activity="go to the office"){
+    return (`This Monday, I will ${activity}.`)
+}
+
+function wrapAdjective(Flare="*"){
+    return function (param="special"){
+        return (`You are ${Flare}${param}${Flare}!`);
+        }
+    }
